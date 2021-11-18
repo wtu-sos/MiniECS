36d019808182a8aa03c5c158a241d1122e66f494
mowen <mowen@gmail.com>
2021-11-18 18:57:09 +0800

    new file

---
+++ MiniECS/MiniECS/EntityBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECS
+{
+    public class EntityBuilder
+    {
+        public static EntityBuilder Default = new EntityBuilder();
+
+        private List<System.ValueType> comps = new List<ValueType>();
+
+        public EntityBuilder AddComponent<T>(T t) where T: struct
+        {
+            comps.Add(t);
+            return this;
+        }
+
+        public void Build(World world)
+        {
+            
+        }
+    }
+}
